75283bbe4b03fb1d09cdef18cb46636ac20e10a8
GeorgeMcKinlay <grgmckinlay@gmail.com>
2016-05-21 21:08:58 +0200

    [add] Terminal colours

--- src/main/java/net/ceriat/clgd/ccemux/emulation/EmuComputer.java
+++ src/main/java/net/ceriat/clgd/ccemux/emulation/EmuComputer.java
@@ -17,7 +17,7 @@ public class EmuComputer implements Closeable {
 
     public EmuComputer(int termWidth, int termHeight, int pixelWidth, int pixelHeight) {
         terminal = new Terminal(termWidth, termHeight);
-        computer = new Computer(new EmuEnvironment(false), terminal, 0);
+        computer = new Computer(new EmuEnvironment(true), terminal, 0);
 
         renderer = new TerminalRenderer(
             CCEmuX.instance.ccAssets.font,
@@ -28,17 +28,42 @@ public class EmuComputer implements Closeable {
         computer.turnOn();
     }
 
+    private static final String BASE_16 = "0123456789abcdef";
+
+    private static Colour getColourFromLineChar(char c) {
+        int i = 15 - BASE_16.indexOf(Character.toLowerCase(c));
+        dan200.computercraft.shared.util.Colour col = dan200.computercraft.shared.util.Colour.fromInt(i);
+
+        if (col == null) {
+            return Colour.WHITE;
+        }
+
+        return new Colour(col.getR(), col.getG(), col.getB());
+    }
+
     public void syncWithRenderer() {
         renderer.startTextUpdate();
 
         for (int y = 0; y < terminal.getHeight(); ++y) {
             for (int x = 0; x < terminal.getWidth(); ++x) {
                 char text = terminal.getLine(y).charAt(x);
-                renderer.updateText(new Point(x, y), Colour.WHITE, text);
+                char textColour = terminal.getTextColourLine(y).charAt(x);
+                renderer.updateText(new Point(x, y), getColourFromLineChar(textColour), text);
             }
         }
 
         renderer.stopTextUpdate();
+
+        renderer.startPixelUpdate();
+
+        for (int y = 0; y < terminal.getHeight(); ++y) {
+            for (int x = 0; x < terminal.getWidth(); ++x) {
+                char colour = terminal.getBackgroundColourLine(y).charAt(x);
+                renderer.updatePixel(new Point(x, y), getColourFromLineChar(colour));
+            }
+        }
+
+        renderer.stopPixelUpdate();
     }
 
     @Override
