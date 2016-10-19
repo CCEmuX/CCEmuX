aec2669cad2b68a7d6f563c8504a1d220848f623
Drew Lemmy <drew@lemmmy.pw>
2016-10-19 20:36:56 +0100

    Added settings to emu.lua

--- src/main/resources/programs/emu.lua
+++ src/main/resources/programs/emu.lua
@@ -1,4 +1,4 @@
-local args = {...}
+local args = {... }
 
 if ccemux then
 	local function help()
@@ -7,6 +7,8 @@ if ccemux then
 		print("emu open - open an emulator with the next ID")
 		print("emu open 2 - open an emulator with ID 2")
 		print("emu data - opens the data folder")
+        print("emu set <setting> <values> - edits a setting")
+        print("emu list settings - list editable settings")
 	end
 	
 	if #args == 0 then
@@ -22,6 +24,30 @@ if ccemux then
 			else
 				print("Unable to open data folder")
 			end
+		elseif args[1] == "set" then
+            if #args <= 1 then
+                help()
+            else
+                if args[2] == "resolution" then
+
+                elseif args[2] == "scale" then
+
+                elseif args[2] == "cursor" then
+                    ccemux.setCursorChar(args[3])
+                    print("Set cursor char to " .. args[3])
+                else
+                    printError("Unrecognized setting: " .. args[2])
+                end
+            end
+        elseif args[1] == "list" then
+            if args[2] == "settings" then
+                print("Editable settings:")
+                print("resolution <width> <height>")
+                print("scale <pixels>")
+                print("cursor <char>")
+            else
+                printError("Unrecognized subcommand: " .. args[2])
+            end
 		else
 			printError("Unrecognized subcommand: " .. args[1])
 			help()
