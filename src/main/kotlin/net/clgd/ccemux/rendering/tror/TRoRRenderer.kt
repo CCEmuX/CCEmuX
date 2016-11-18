07caec94f2e600fcd7867b0047b16a70da11c803
apemanzilla <dominic@marcuse.us>
2016-11-18 17:39:51 -0500

    Fix TRoR

--- src/main/kotlin/net/clgd/ccemux/rendering/tror/TRoRRenderer.kt
+++ src/main/kotlin/net/clgd/ccemux/rendering/tror/TRoRRenderer.kt
@@ -1,6 +1,5 @@
 package net.clgd.ccemux.rendering.tror
 
-import net.clgd.ccemux.Utils.parseInt
 import net.clgd.ccemux.emulation.CCEmuX
 import net.clgd.ccemux.emulation.EmulatedComputer
 import net.clgd.ccemux.emulation.tror.CapabilitiesPacket
@@ -10,42 +9,8 @@ import net.clgd.ccemux.emulation.tror.TRoRTerminal
 import net.clgd.ccemux.rendering.Renderer
 import java.io.InputStream
 import java.io.OutputStream
-import java.util.*
-import java.util.regex.Matcher
-import java.util.regex.Pattern
 
 class TRoRRenderer(val emu: CCEmuX, val ec: EmulatedComputer, private val outputStream: OutputStream, inputStream: InputStream) : Renderer {
-	companion object {
-		val eventPattern = Pattern.compile("^EV:([^;]*);(.*)$")
-
-		val handlers = HashMap<Pattern, (EmulatedComputer, Matcher) -> Unit>()
-
-		init {
-			// char
-			handlers.put(
-					Pattern.compile("^\"char\",\"(.)\""),
-					{ ec, m -> ec.pressChar(m.group(1).getOrElse(0, { ' ' })) }
-			)
-
-			// key
-			handlers.put(
-					Pattern.compile("^\"key\",(\\d+)"),
-					{ ec, m -> ec.pressKey(m.group(1).parseInt(), false) }
-			)
-
-			// key_up
-			handlers.put(
-					Pattern.compile("^\"key_up\",(\\d+)"),
-					{ ec, m -> ec.pressKey(m.group(1).parseInt(), true) }
-			)
-
-			// paste
-			handlers.put(
-					Pattern.compile("^\"paste\",\"(.+)\""),
-					{ ec, m -> ec.pasteText(m.group(1)) }
-			)
-		}
-	}
 
 	val id = ec.getID().toString()
 	val term = if (ec.terminal is TRoRTerminal) {
@@ -54,12 +19,10 @@ class TRoRRenderer(val emu: CCEmuX, val ec: EmulatedComputer, private val output
 		throw IllegalArgumentException("Cannot create TRoR renderer unless a TRoR terminal is used")
 	}
 
-	val input = AsyncScanner(inputStream)
-
 	var firstsend = true
 	var paused = true
 	var prefix = ""
-
+	
 	constructor(emu: CCEmuX, ec: EmulatedComputer) : this(emu, ec, System.out, System.`in`) {
 		prefix = "[TRoR]"
 	}
@@ -85,22 +48,6 @@ class TRoRRenderer(val emu: CCEmuX, val ec: EmulatedComputer, private val output
 			}
 
 			term.popQueue().forEach { t -> send(t) }
-
-			while (input.hasLines()) {
-				input.lines.forEach {
-					it -> run {
-						val m = TRoRRenderer.eventPattern.matcher(it)
-						if (m?.group(1) == id) {
-							handlers.forEach { p, f ->
-								val m2 = TRoRRenderer.eventPattern.matcher(m.group(2))
-								if (m2 != null) {
-									f(ec, m2)
-								}
-							}
-						}
-					}
-				}
-			}
 		}
 	}
 
