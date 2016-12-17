4458da2ec21a97873c09edc8ea71eee9ad915e27
Lignum <aut@ism.pw>
2016-12-17 18:15:00 +0100

    Add ccemux.setClipboard(contents)

--- src/main/java/net/clgd/ccemux/emulation/CCEmuXAPI.java
+++ src/main/java/net/clgd/ccemux/emulation/CCEmuXAPI.java
@@ -1,6 +1,8 @@
 package net.clgd.ccemux.emulation;
 
-import java.awt.Desktop;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -15,7 +17,7 @@ import net.clgd.ccemux.rendering.RenderingMethod;
 
 @FunctionalInterface
 interface APIMethod {
-	public Object[] accept(Object[] o) throws LuaException;
+	Object[] accept(Object[] o) throws LuaException;
 }
 
 public class CCEmuXAPI implements ILuaAPI {
@@ -23,33 +25,33 @@ public class CCEmuXAPI implements ILuaAPI {
 	private String name;
 	private EmulatedComputer computer;
 	private final Map<String, APIMethod> methods = new LinkedHashMap<>();
-	
+
 	{
 		methods.put("getVersion", o -> new Object[] {CCEmuX.getVersion()});
-		
+
 		methods.put("setCursorChar", o -> {
 			if (o.length < 1 || !(o[0] instanceof String)) {
 				throw new LuaException("expected string for argument #1");
 			}
-			
+
 			String s = (String) o[0];
 			if (s.length() < 1) {
 				throw new LuaException("cursor char cannot be empty");
 			}
-			
+
 			computer.cursorChar = s.charAt(0);
-			
+
 			return new Object[] {};
 		});
-		
+
 		methods.put("closeEmu", o -> {
 			computer.dispose();
 			return new Object[] {};
 		});
-		
+
 		methods.put("openEmu", o -> {
 			int id = -1;
-			
+
 			if (o.length > 0 && o[0] != null) {
 				if (o[0] instanceof Number) {
 					id = ((Number)o[0]).intValue();
@@ -57,17 +59,17 @@ public class CCEmuXAPI implements ILuaAPI {
 					throw new LuaException("expected number or nil for argument #1");
 				}
 			}
-			
+
 			EmulatedComputer ec = computer.emu.createEmulatedComputer(id, null);
 			List<Renderer> r = ec.emu.conf.getRenderer().stream()
 					.map(s -> RenderingMethod.create(s, ec))
 					.collect(Collectors.toList());
-			
+
 			r.forEach(_r -> _r.setVisible(true));
-			
+
 			return new Object[] {ec.getID()};
 		});
-		
+
 		methods.put("openDataDir", o -> {
 			try {
 				Desktop.getDesktop().browse(computer.emu.dataDir.toUri());
@@ -76,31 +78,43 @@ public class CCEmuXAPI implements ILuaAPI {
 				return new Object[] {false, e.toString()};
 			}
 		});
-		
+
 		methods.put("milliTime", o -> {
 			return new Object[] {System.currentTimeMillis()};
 		});
-		
+
 		methods.put("nanoTime", o -> {
 			return new Object[] {System.nanoTime()};
 		});
-		
+
 		methods.put("echo", o -> {
 			if (o.length > 0 && o[0] instanceof String) {
 				computer.emu.logger.info("[Computer {}] {}", computer.getID(), (String)o[0]);
 			} else {
 				throw new LuaException("expected string for argument #1");
 			}
-			
+
+			return new Object[] {};
+		});
+
+		methods.put("setClipboard", o -> {
+			if (o.length > 0 && o[0] instanceof String) {
+				StringSelection sel = new StringSelection((String) o[0]);
+				Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
+				c.setContents(sel, sel);
+			} else {
+				throw new LuaException("expected string for argument #1");
+			}
+
 			return new Object[] {};
 		});
 	}
-	
+
 	public CCEmuXAPI(EmulatedComputer computer, String name) {
 		this.computer = computer;
 		this.name = name;
 	}
-	
+
 	@Override
 	public String[] getMethodNames() {
 		return methods.keySet().toArray(new String[] {});
