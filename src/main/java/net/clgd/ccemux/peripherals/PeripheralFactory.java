f51e3bce2b758363205870538c8c4a7c6e9437cc
Justyn <justync9@gmail.com>
2017-05-04 23:04:15 -0500

    Remove pointless PluginPeripheral and switch to IPeripheral

--- src/main/java/net/clgd/ccemux/peripherals/PeripheralFactory.java
+++ src/main/java/net/clgd/ccemux/peripherals/PeripheralFactory.java
@@ -1,10 +1,11 @@
 package net.clgd.ccemux.peripherals;
 
+import dan200.computercraft.api.peripheral.IPeripheral;
 import net.clgd.ccemux.emulation.EmulatedComputer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public interface PeripheralFactory {
-	public static final Map<String, PluginPeripheral> implementations = new HashMap<>();
+	public static final Map<String, IPeripheral> implementations = new HashMap<>();
 }
