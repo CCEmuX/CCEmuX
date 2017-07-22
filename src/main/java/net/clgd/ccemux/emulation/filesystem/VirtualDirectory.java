ac225422d7e6e16937d54b9921d003f4285de2d9
apemanzilla <dominic@marcuse.us>
2017-07-21 23:57:23 -0400

    Add VirtualMount and related code

---
+++ src/main/java/net/clgd/ccemux/emulation/filesystem/VirtualDirectory.java
@@ -0,0 +1,98 @@
+package net.clgd.ccemux.emulation.filesystem;
+
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import lombok.EqualsAndHashCode;
+
+/**
+ * Represents an immutable, in-memory directory for use with
+ * {@link VirtualMount}
+ * 
+ * @author apemanzilla
+ *
+ */
+@EqualsAndHashCode
+public final class VirtualDirectory implements VirtualMountEntry {
+	/**
+	 * A builder for {@link VirtualDirectory} that automatically creates the
+	 * necessary directories for each entry
+	 * 
+	 * @author apemanzilla
+	 *
+	 */
+	public static class Builder {
+		private VirtualDirectory dir = new VirtualDirectory();
+
+		public void addEntry(Path path, VirtualMountEntry entry) {
+			path = path.normalize();
+
+			if (path.getNameCount() == 0) {
+				throw new IllegalArgumentException("Invalid path");
+			} else if (path.getNameCount() == 1) {
+				dir.children.put(path.getFileName().toString(), entry);
+			} else {
+				VirtualDirectory current = dir;
+
+				Iterator<Path> i = path.iterator();
+				Path p;
+				while ((p = i.next()) != null) {
+					String name = p.getFileName().toString();
+					if (i.hasNext()) {
+						if (current.hasEntry(name) && current.getEntry(name) instanceof VirtualDirectory) {
+							current = (VirtualDirectory) current.getEntry(name);
+						} else {
+							VirtualDirectory next = new VirtualDirectory();
+							current.children.put(name, next);
+							current = next;
+						}
+					} else {
+						current.children.put(name, entry);
+						break;
+					}
+				}
+			}
+		}
+
+		public VirtualDirectory build() {
+			if (dir == null) throw new IllegalStateException("Builder has already been used!");
+			VirtualDirectory out = dir;
+			dir = null;
+			return out;
+		}
+	}
+
+	private final Map<String, VirtualMountEntry> children;
+
+	/**
+	 * Creates a new directory
+	 * 
+	 * @param children
+	 *            The entries contained in this directory
+	 */
+	public VirtualDirectory(Map<String, VirtualMountEntry> children) {
+		this.children = children;
+	}
+
+	/**
+	 * Creates a new directory with no children
+	 */
+	public VirtualDirectory() {
+		this.children = new HashMap<>();
+	}
+
+	public boolean hasEntry(String name) {
+		return children.containsKey(name);
+	}
+
+	public VirtualMountEntry getEntry(String name) {
+		return children.get(name);
+	}
+
+	public Set<String> getEntryNames() {
+		return children.keySet();
+	}
+}
---
+++ src/main/java/net/clgd/ccemux/emulation/filesystem/VirtualMount.java
@@ -0,0 +1,94 @@
+package net.clgd.ccemux.emulation.filesystem;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import dan200.computercraft.api.filesystem.IMount;
+import lombok.Getter;
+
+/**
+ * An immutable, in-memory {@link dan200.computercraft.api.filesystem.IMount
+ * IMount} implementation.
+ * 
+ * @author apemanzilla
+ *
+ */
+public class VirtualMount implements IMount {
+	@Getter
+	private final VirtualDirectory root;
+
+	/**
+	 * Constructs a new <code>VirtualMount</code> with the given
+	 * {@link net.clgd.ccemux.emulation.filesystem.VirtualDirectory
+	 * VirtualDirectory} as its root folder.
+	 * 
+	 * @param root
+	 */
+	public VirtualMount(VirtualDirectory root) {
+		this.root = root;
+	}
+
+	/**
+	 * Follows the given path to get a
+	 * {@link net.clgd.ccemux.emulation.filesystem.VirtualMountEntry
+	 * MountEntry}. Returns <code>null</code> if the path is invalid (e.g.
+	 * non-existent entry or trying to get child of a directory)
+	 * 
+	 * @param path
+	 *            The path to follow
+	 * @return The entry at the given path, or <code>null</code> if the path is
+	 *         invalid
+	 */
+	public VirtualMountEntry follow(Path path) {
+		if (path.normalize().equals(Paths.get(""))) return root;
+
+		VirtualMountEntry current = getRoot();
+		for (Path p : path.normalize()) {
+			if (!(current instanceof VirtualDirectory)) return null;
+			current = ((VirtualDirectory) current).getEntry(p.getName(0).toString());
+			if (current == null) return null;
+		}
+		return current;
+	}
+
+	@Override
+	public boolean exists(String path) {
+		return follow(Paths.get(path)) != null;
+	}
+
+	@Override
+	public long getSize(String path) throws IOException {
+		VirtualMountEntry e = follow(Paths.get(path));
+		if (e == null) throw new IOException("No such file or directory");
+		return e instanceof VirtualFile ? ((VirtualFile) e).length() : 0;
+	}
+
+	@Override
+	public boolean isDirectory(String path) {
+		return follow(Paths.get(path)) instanceof VirtualDirectory;
+	}
+
+	@Override
+	public void list(String path, List<String> names) throws IOException {
+		VirtualMountEntry e = follow(Paths.get(path));
+		if (e instanceof VirtualDirectory) {
+			names.addAll(((VirtualDirectory) e).getEntryNames());
+		} else {
+			throw new IOException("Cannot list children of non-directory");
+		}
+	}
+
+	@Override
+	public InputStream openForRead(String path) throws IOException {
+		VirtualMountEntry e = follow(Paths.get(path));
+		if (e instanceof VirtualFile) {
+			return new ByteArrayInputStream(((VirtualFile) e).getData());
+		} else {
+			throw new IOException("Only files can be read");
+		}
+	}
+}
