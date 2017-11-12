fd2625f2733fbeb0a841b694a37c7b34119da1ac
SquidDev <bonzoweb@hotmail.co.uk>
2017-11-12 08:34:22 +0000

    Prefer primitive types in property definitions

--- src/main/java/net/clgd/ccemux/emulation/EmuConfig.java
+++ src/main/java/net/clgd/ccemux/emulation/EmuConfig.java
@@ -17,18 +17,18 @@ public abstract class EmuConfig extends Config {
 
 	public abstract Path getDataDir();
 
-	public Property<Double> termScale = property("termScale", Double.class, 2.0)
+	public Property<Double> termScale = property("termScale", double.class, 2.0)
 			.setName("Terminal scale");
-	public Property<Integer> termHeight = property("termHeight", Integer.class, 19)
+	public Property<Integer> termHeight = property("termHeight", int.class, 19)
 			.setName("Terminal height");
-	public Property<Integer> termWidth = property("termWidth", Integer.class, 52)
+	public Property<Integer> termWidth = property("termWidth", int.class, 52)
 			.setName("Terminal width");
 	public Property<String> renderer = property("renderer", String.class, "AWT")
 			.setName("Renderer");
 
-	public Property<Long> maxComputerCapacity = property("maxComputerCapacity", Long.class, 2L * 1024 * 1024)
+	public Property<Long> maxComputerCapacity = property("maxComputerCapacity", long.class, 2L * 1024 * 1024)
 			.setName("Max computer capacity");
-	public Property<Boolean> httpEnabled = property("httpEnable", Boolean.class, true)
+	public Property<Boolean> httpEnabled = property("httpEnable", boolean.class, true)
 			.setName("Enable HTTP API")
 			.setDescription("Enable the \"http\" API on Computers (see \"httpWhitelist\" and \"httpBlacklist\" for more fine grained control than this)");
 	public Property<String[]> httpWhitelist = property("httpWhitelist", String[].class, new String[]{"*"})
@@ -41,7 +41,7 @@ public abstract class EmuConfig extends Config {
 			.setDescription("A list of wildcards for domains or IP ranges that cannot be accessed through the \"http\" API on Computers.\n" +
 					"If this is empty then all whitelisted domains will be accessible. Example: \"*.github.com\" will block access to all subdomains of github.com.\n" +
 					"You can use domain names (\"pastebin.com\"), wilcards (\"*.pastebin.com\") or CIDR notation (\"127.0.0.0/8\").");
-	public Property<Boolean> disableLua51Features = property("disableLua51Features", Boolean.class, false)
+	public Property<Boolean> disableLua51Features = property("disableLua51Features", boolean.class, false)
 			.setName("Disable Lua 5.1 features")
 			.setDescription("Set this to true to disable Lua 5.1 functions that will be removed in a future update. Useful for ensuring forward compatibility of your programs now.");
 	public Property<String> defaultComputerSettings = property("defaultComputerSettings", String.class, "")
