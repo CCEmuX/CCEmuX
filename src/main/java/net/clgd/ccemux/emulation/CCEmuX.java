09e90dd77bdfd116bc6c76ef0a41687a221632d3
Jonathan Coates <bonzoweb@hotmail.co.uk>
2020-04-30 18:08:13 +0100

    Rewrite how mounts are injected (#136)
     - Move IComputerEnvironment implementation to a separate class.
     - Instead of injecting mounts by setting a field, we override
       createSaveDirMount to either construct a standard mount, or use the
       custom supplier.
    
       This means that computers are created (and thus rendered)
       immediately, rather than waiting for file scanning to complete.
       Closes #132

--- src/main/java/net/clgd/ccemux/emulation/CCEmuX.java
+++ src/main/java/net/clgd/ccemux/emulation/CCEmuX.java
@@ -1,8 +1,9 @@
 package net.clgd.ccemux.emulation;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
@@ -15,21 +16,11 @@ import javax.annotation.Nonnull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import dan200.computercraft.ComputerCraft;
-import dan200.computercraft.api.filesystem.IMount;
-import dan200.computercraft.api.filesystem.IWritableMount;
-import dan200.computercraft.core.computer.IComputerEnvironment;
-import dan200.computercraft.core.filesystem.ComboMount;
-import dan200.computercraft.core.filesystem.FileMount;
-import dan200.computercraft.core.filesystem.JarMount;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import net.clgd.ccemux.api.emulation.EmuConfig;
 import net.clgd.ccemux.api.emulation.EmulatedComputer;
 import net.clgd.ccemux.api.emulation.EmulatedTerminal;
 import net.clgd.ccemux.api.emulation.Emulator;
-import net.clgd.ccemux.api.emulation.filesystem.VirtualDirectory;
-import net.clgd.ccemux.api.emulation.filesystem.VirtualMount;
 import net.clgd.ccemux.api.peripheral.PeripheralFactory;
 import net.clgd.ccemux.api.rendering.Renderer;
 import net.clgd.ccemux.api.rendering.RendererFactory;
@@ -37,7 +28,7 @@ import net.clgd.ccemux.init.UserConfig;
 import net.clgd.ccemux.plugins.PluginManager;
 
 @RequiredArgsConstructor
-public class CCEmuX implements Runnable, Emulator, IComputerEnvironment {
+public class CCEmuX implements Runnable, Emulator {
 	private static final Logger log = LoggerFactory.getLogger(CCEmuX.class);
 
 	public static String getVersion() {
@@ -71,7 +62,7 @@ public class CCEmuX implements Runnable, Emulator, IComputerEnvironment {
 
 	@Nonnull
 	@Override
-	public EmuConfig getConfig() {
+	public UserConfig getConfig() {
 		return cfg;
 	}
 
@@ -250,85 +241,7 @@ public class CCEmuX implements Runnable, Emulator, IComputerEnvironment {
 		return (System.currentTimeMillis() - started) / 50;
 	}
 
-	@Override
 	public int assignNewID() {
 		return nextID++;
-	}
-
-	@Override
-	public IMount createResourceMount(String domain, String subPath) {
-		String path = Paths.get("assets", domain, subPath).toString().replace('\\', '/');
-		if (path.startsWith("/")) path = path.substring(1);
-
-		JarMount jarMount;
-		try {
-			jarMount = new JarMount(ccSource, path);
-		} catch (IOException e) {
-			log.error("Could not create mount from mod jar", e);
-			return null;
-		}
-
-		VirtualDirectory.Builder romBuilder = new VirtualDirectory.Builder();
-		pluginMgr.onCreatingROM(this, romBuilder);
-
-		return new ComboMount(new IMount[] {
-			// From ComputerCraft JAR
-			jarMount,
-			// From plugin files
-			new VirtualMount(romBuilder.build()),
-			// From data directory
-			new FileMount(cfg.getAssetDir().resolve(path).toFile(), 0)
-		});
-	}
-
-	@Override
-	public InputStream createResourceFile(String domain, String subPath) {
-		String path = Paths.get("assets", domain, subPath).toString().replace('\\', '/');
-		if (path.startsWith("/")) path = path.substring(1);
-
-		File assetFile = cfg.getAssetDir().resolve(path).toFile();
-		if (assetFile.exists() && assetFile.isFile()) {
-			try {
-				return new FileInputStream(assetFile);
-			} catch (FileNotFoundException e) {
-				log.error("Failed to create resource file", e);
-			}
-		}
-
-		return CCEmuX.class.getClassLoader().getResourceAsStream(path);
-	}
-
-	@Override
-	public IWritableMount createSaveDirMount(String path, long capacity) {
-		return new FileMount(cfg.getComputerDir().resolve(path).toFile(), getComputerSpaceLimit());
-	}
-
-	@Override
-	public long getComputerSpaceLimit() {
-		return cfg.maxComputerCapacity.get();
-	}
-
-	@Override
-	public int getDay() {
-		return (int) (((getTicksSinceStart() + 6000) / 24000) + 1);
-	}
-
-	@Override
-	public String getHostString() {
-		if (getVersion() != null) {
-			return String.format("ComputerCraft %s (CCEmuX %s)", ComputerCraft.getVersion(), getVersion());
-		} else {
-			return String.format("ComputerCraft %s (CCEmuX)", ComputerCraft.getVersion());
-		}
-	}
-
-	@Override
-	public double getTimeOfDay() {
-		return ((getTicksSinceStart() + 6000) % 24000) / 1000d;
-	}
-
-	@Override
-	public boolean isColour() {
-		return true;
 	}
 }
---
+++ src/main/java/net/clgd/ccemux/emulation/ComputerEnvironment.java
@@ -0,0 +1,125 @@
+package net.clgd.ccemux.emulation;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dan200.computercraft.ComputerCraft;
+import dan200.computercraft.api.filesystem.IMount;
+import dan200.computercraft.api.filesystem.IWritableMount;
+import dan200.computercraft.core.computer.IComputerEnvironment;
+import dan200.computercraft.core.filesystem.ComboMount;
+import dan200.computercraft.core.filesystem.FileMount;
+import dan200.computercraft.core.filesystem.JarMount;
+import net.clgd.ccemux.api.emulation.filesystem.VirtualDirectory;
+import net.clgd.ccemux.api.emulation.filesystem.VirtualMount;
+
+class ComputerEnvironment implements IComputerEnvironment {
+	private static final Logger log = LoggerFactory.getLogger(ComputerEnvironment.class);
+
+	private final CCEmuX emu;
+	private final int id;
+	private final Supplier<IWritableMount> mount;
+
+	ComputerEnvironment(CCEmuX emu, int id, Supplier<IWritableMount> mount) {
+		this.emu = emu;
+		this.id = id;
+		this.mount = mount;
+	}
+
+	@Override
+	public long getComputerSpaceLimit() {
+		return emu.getConfig().maxComputerCapacity.get();
+	}
+
+	@Override
+	public int getDay() {
+		return (int) (((emu.getTicksSinceStart() + 6000) / 24000) + 1);
+	}
+
+	@Override
+	public String getHostString() {
+		String version = CCEmuX.getVersion();
+		if (version != null) {
+			return String.format("ComputerCraft %s (CCEmuX %s)", ComputerCraft.getVersion(), version);
+		} else {
+			return String.format("ComputerCraft %s (CCEmuX)", ComputerCraft.getVersion());
+		}
+	}
+
+	@Override
+	public int assignNewID() {
+		return emu.assignNewID();
+	}
+
+
+	@Override
+	public IMount createResourceMount(String domain, String subPath) {
+		String path = Paths.get("assets", domain, subPath).toString().replace('\\', '/');
+		if (path.startsWith("/")) path = path.substring(1);
+
+		JarMount jarMount;
+		try {
+			jarMount = new JarMount(emu.getCCJar(), path);
+		} catch (IOException e) {
+			log.error("Could not create mount from mod jar", e);
+			return null;
+		}
+
+		VirtualDirectory.Builder romBuilder = new VirtualDirectory.Builder();
+		emu.getPluginMgr().onCreatingROM(emu, romBuilder);
+
+		return new ComboMount(new IMount[] {
+			// From ComputerCraft JAR
+			jarMount,
+			// From plugin files
+			new VirtualMount(romBuilder.build()),
+			// From data directory
+			new FileMount(emu.getConfig().getAssetDir().resolve(path).toFile(), 0)
+		});
+	}
+
+	@Override
+	public InputStream createResourceFile(String domain, String subPath) {
+		String path = Paths.get("assets", domain, subPath).toString().replace('\\', '/');
+		if (path.startsWith("/")) path = path.substring(1);
+
+		File assetFile = emu.getConfig().getAssetDir().resolve(path).toFile();
+		if (assetFile.exists() && assetFile.isFile()) {
+			try {
+				return new FileInputStream(assetFile);
+			} catch (FileNotFoundException e) {
+				log.error("Failed to create resource file", e);
+			}
+		}
+
+		return CCEmuX.class.getClassLoader().getResourceAsStream(path);
+	}
+
+	@Override
+	public IWritableMount createSaveDirMount(String path, long capacity) {
+		// createSaveDirMount should only be called with computer/$id.
+		if (!path.equals("computer/" + id)) {
+			log.error("Unexpected call to createSaveDirMount for {}", path);
+			return new FileMount(emu.getConfig().getComputerDir().resolve(path).toFile(), capacity);
+		}
+
+		return Optional.ofNullable(mount)
+			.map(Supplier::get)
+			.orElseGet(() -> new FileMount(emu.getConfig().getComputerDir().resolve(Integer.toString(id)).toFile(), capacity));
+	}
+
+	@Override
+	public double getTimeOfDay() {
+		return ((emu.getTicksSinceStart() + 6000) % 24000) / 1000d;
+	}
+
+	@Override
+	public boolean isColour() {
+		return true;
+	}
+}
