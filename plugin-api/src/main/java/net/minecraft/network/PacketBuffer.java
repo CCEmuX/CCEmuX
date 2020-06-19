5be224f48049c3d8bf60bca638209fae9973cfe0
SquidDev <git@squiddev.cc>
2020-06-19 13:45:40 +0100

    Bump Gradle to 6.5 and CC:T to 1.89.0
    This is a tiny bit ugly as we need a stub file, but otherwise it's fine.

---
+++ plugin-api/src/main/java/net/minecraft/network/PacketBuffer.java
@@ -0,0 +1,11 @@
+package net.minecraft.network;
+
+/**
+ * A stub class for Minecraft's packet buffer.
+ *
+ * @deprecated This is just a stub to enable some code to compile. It should not
+ * be used.
+ */
+@Deprecated
+public class PacketBuffer {
+}
