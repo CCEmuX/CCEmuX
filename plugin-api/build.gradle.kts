50cf57aea24b5e85c348c0373e29adf335987fd5
Jonathan Coates <git@squiddev.cc>
2023-04-05 21:13:15 +0100

    Update CC:T to 1.104.0

--- plugin-api/build.gradle.kts
+++ plugin-api/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 val ccVersion: String by extra
 
 dependencies {
-	api("cc.tweaked:cc-tweaked-1.19.3-core:$ccVersion")
+	api("cc.tweaked:cc-tweaked-1.19.4-core:$ccVersion")
 	api("org.slf4j:slf4j-api:2.0.6")
 	api("com.google.guava:guava:31.1-jre")
 }
